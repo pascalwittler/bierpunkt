c3d7f9fe446f362e02b19d4e6f7372f488e010d6
Pascal Wittler <git@pascalwittler.de>
2022-07-16 20:13:24 +0200

    add index route as example + function check to api

--- api/app.py
+++ api/app.py
@@ -2,3 +2,10 @@ from flask import Flask, g
 from flask_restful import Resource, Api, reqparse
 app = Flask(__name__)
 api = Api(app)
+
+@app.route('/')
+def index():
+    return {'message': 'Hello world!', 'data': {}}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2342, debug=True)
